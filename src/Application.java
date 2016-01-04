719706cda630a3c039672fff889dc9cc175c1a1a
warzesia <magdalena.warzecha@websudos.co.uk>
2016-01-04 23:44:18 +0000

    creating first json pattern...

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,11 @@
+import evolution.ElementFactory;
+import evolution.LayoutFactory;
+import evolution.Population;
 import tools.JsonTestParser;
+import tools.Params;
+import treeComponents.SVGNode;
+import view.MainView;
+import view.SVGCreator;
 
 /**
  * Created by warzesia on 30/11/15.
@@ -7,22 +14,22 @@ public class Application {
 
     public static void main(String[] args) {
 
-//        Population population = new Population();
-//        population.generate(Params.POPULATION_SIZE);
-//
-//        SVGNode rootNode = LayoutFactory.getRandomRootNode();
-//        rootNode.generate();
-//
-//        SVGCreator svgCreator = new SVGCreator();
-//        svgCreator.drawSVGTree(rootNode);
-//
-//        MainView.setSVGDocument(svgCreator.getSVGDocument());
-//
-//        System.out.println("bla!");
-//
-//        ElementFactory.getRandomImage();
+        Population population = new Population();
+        population.generate(Params.POPULATION_SIZE);
 
-        JsonTestParser.run();
+        SVGNode rootNode = LayoutFactory.getRandomRootNode();
+        rootNode.generate();
+
+        SVGCreator svgCreator = new SVGCreator();
+        svgCreator.drawSVGTree(rootNode);
+
+        MainView.setSVGDocument(svgCreator.getSVGDocument());
+
+        System.out.println("bla!");
+
+        ElementFactory.getRandomImage();
+
+//        JsonTestParser.run();
 
 
 
